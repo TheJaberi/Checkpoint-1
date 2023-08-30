675547193edb00b602a4c71ce5a073988e0f3377
TmAppRep <tm.app.adm@gmail.com>
2023-08-30 21:54:20 +0300

    foldint

--- foldint/foldint.go
+++ foldint/foldint.go
@@ -1,9 +1,27 @@
 package piscine
 
+import "github.com/01-edu/z01"
+
 func FoldInt(f func(int, int) int, a []int, n int) {
 	result := n
 	for _, num := range a {
-		result = f(result,num)
+		result = f(result, num)
 	}
-	fmt.Println(result)
+	PrintStringln(Itoa(result))
+}
+
+func Itoa(n int) string {
+	s := ""
+	for n != 0 {
+		s = string(n%10+'0') + s
+		n /= 10
+	}
+	return s
+}
+
+func PrintStringln(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
 }
